402da88027f01b26cfca08c8dc148641e20c729c
Tony <tonghuan.li0@gmail.com>
2018-11-23 14:55:06 +0800

    通过计算lable里面字体的高度，来计算lable

--- UI技巧篇/01-UILable文字顶部对齐/01-UILable文字顶部对齐/ViewController.m
+++ UI技巧篇/01-UILable文字顶部对齐/01-UILable文字顶部对齐/ViewController.m
@@ -17,6 +17,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *testLb = [[UILabel alloc] init];
+    NSString *testString = @"My name is Tony Li.And what a awesome day!Today what are you doing now?";
+    testLb.text = testString;
+    testLb.numberOfLines = 0;
+    testLb.backgroundColor = [UIColor lightGrayColor];
+    testLb.textAlignment = NSTextAlignmentLeft;
+    UIFont *font = [UIFont boldSystemFontOfSize:14.0];
+    testLb.font = font;
+    NSDictionary *attrs = @{NSFontAttributeName: font};
+    // 宽度固定，高度随字体
+    CGSize maximumSize = CGSizeMake(300, MAXFLOAT);
+    CGRect testSize = [testString boundingRectWithSize:maximumSize options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil];
+    CGRect testFrame = CGRectMake(10, 64 + 10, 300, testSize.size.height);
+    testLb.frame = testFrame;
+    [self.view addSubview:testLb];
+    
 }
 
 
