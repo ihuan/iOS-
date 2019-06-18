a84040df2bafbeefb65ea0c8e956a4de80faa9ca
Tony <tonghuan.li0@gmail.com>
2019-06-18 11:32:58 +0800

    主题更改，测试完成

--- 换肤/ThemeDemo/ThemeDemo/ViewController.swift
+++ 换肤/ThemeDemo/ThemeDemo/ViewController.swift
@@ -15,31 +15,43 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.darkGray
         
-        let themeBtn = UIButton.init(type: .custom)
-        themeBtn.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
-        themeBtn.center = view.center
-        themeBtn.setTitleColor(UIColor.lightGray, for: .highlighted)
-        themeBtn.setTitleColor(UIColor.black, for: [])
-        themeBtn.setTitle("点击", for: [])
-        themeBtn.addTarget(self, action: #selector(changeThemeAction), for: .touchUpInside)
-        themeBtn.tnBackGroundColor = TNThemeManager.shared.getCurrentColor(theme: "BG")
-        themeBtn.layer.cornerRadius = 5.0
-//        themeBtn.backgroundColor = UIColor(red: 34/255, green: 232/255, blue: 23/255, alpha: 1.0)
-        view.addSubview(themeBtn)
+        let bgView: UIView = {
+            let v = UIView()
+            v.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - 64*2)
+            v.backgroundColor = v.superview?.backgroundColor
+            v.center = view.center
+            return v
+        } ()
+        view.addSubview(bgView)
         
+        let margin = 24
+        let btnRect = 100
+        for i in 0..<18 {
+            let themeBtn = UIButton.init(type: .custom)
+            themeBtn.frame = CGRect(x: (i%3 + 1)*margin + ((i)%3)*(btnRect),
+                                    y: (i/3+1)*(margin) + (i/3)*(btnRect),
+                                    width: btnRect,
+                                    height: btnRect)
+            themeBtn.setTitleColor(UIColor.lightGray, for: .highlighted)
+            themeBtn.setTitleColor(UIColor.black, for: [])
+            themeBtn.setTitle("更改主题", for: [])
+            themeBtn.addTarget(self, action: #selector(changeThemeAction), for: .touchUpInside)
+            themeBtn.tnBackGroundColor = TNThemeManager.shared.getCurrentColor(theme: "BG")
+            themeBtn.layer.cornerRadius = 5.0
+            bgView.addSubview(themeBtn)
+        }
+        
+      
     }
 
     @objc func changeThemeAction() {
-        print("改变主题")
         if TNThemeManager.shared.currentTheme == "RED" {
             TNThemeManager.shared.currentTheme = "NIGHT"
+        } else if TNThemeManager.shared.currentTheme == "NIGHT" {
+            TNThemeManager.shared.currentTheme = "NORMAL"
         } else {
             TNThemeManager.shared.currentTheme = "RED"
         }
-    }
-    
-    deinit {
-        print("what")
     }
 }
 
